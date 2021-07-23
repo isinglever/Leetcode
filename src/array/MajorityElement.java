cb0fb9aff04b7f3efeb96c8625dc90af9296a0a4
Orikuo <isinglever@gmail.com>
2021-07-23 21:21:07 +0800

    publish my wrong answer code, and solution copied from Leetcode

---
+++ src/array/MajorityElement.java
@@ -0,0 +1,38 @@
+package array;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+
+        /*************************
+        int n = nums.length;
+        int ans = 0;
+        int count = 0;
+        int i = 0;
+        while (i < n) {
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == nums[i]) {
+                  count++;
+                }
+            }
+            if (count > n/2) {
+                ans = nums[i];
+                break;
+            }
+            i++;
+        }
+        return ans;
+         ******Wrong Answer******/
+
+        int major=nums[0], count = 1;
+        for (int i=1; i<nums.length;i++) {
+            if (count==0){
+                count++;
+                major=nums[i];
+            } else if (major==nums[i]) {
+                count++;
+            } else count--;
+
+        }
+        return major;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
-
+import array.MajorityElement;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+           // int[] test = new int[5];
+           int[] test = {7,7,7,9,9};
+         //  new MajorityElement() = MajorityElement(test);
     }
 }
