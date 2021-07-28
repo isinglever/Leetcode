f9eaa4837cc3259be890846885a6988f0359a60b
Orikuo <isinglever@gmail.com>
2021-07-28 09:55:41 +0800

    Greedy Algorithm

---
+++ src/array/AssignCookies.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        /*************rubbish******************
+
+         int n = 0;
+        for (int i = 0; i < s.length; i++) {
+            for (int j = i; j < g.length; j++) {
+                if (s[i] >= g[j]) {
+                    n++;
+                }
+            }
+        }
+        return n;
+         ***********************/
+        int i = 0;
+        for(int j=0;i<g.length && j<s.length;j++) {
+            if(g[i]<=s[j]) i++;
+        }
+        return i;
+    }
+}
