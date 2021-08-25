495df94a58355e5e81ad51c1aaf066a29c74f604
Orikuo <isinglever@gmail.com>
2021-08-25 16:52:09 +0800

    restructure directory

---
+++ Array/03LongestSubstring.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class LongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+
+    }
+}
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
