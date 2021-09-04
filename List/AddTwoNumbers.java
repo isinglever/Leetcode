f2854ba4053f51e3ec801b4fe19b626aa0f104d1
Orikuo <isinglever@gmail.com>
2021-09-04 16:50:37 +0800

    add AddTwoNumbers solution. By the way, this answer is copied from Leetcode

---
+++ List/AddTwoNumbers.java
@@ -0,0 +1,38 @@
+public class AddTwoNumbers {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        ListNode ans = new ListNode();
+//        ans.val = l1.val + l2.val;
+//        if (ans.val > 9) {
+//            ans.val = ans.val - 10;
+//            ans.next.val = ans.next.val + 1;
+//        }
+//        while (l1.next != null && l2.next != null) {
+//            ans.next.val = l1.next.val + l2.next.val;
+//        }
+//        return ans;
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
