978f3e29ff3fbc1c94bcc67ac04e8c7ccc6f60fb
Orikuo <isinglever@gmail.com>
2021-09-10 20:13:33 +0800

    begin offer

--- List/CQueue.java
+++ List/CQueue.java
@@ -1,13 +1,13 @@
 import java.util.Deque;
 import java.util.LinkedList;
-
+/****************************
 public class CQueue {
     Deque<Integer> stack1;
     Deque<Integer> stack2;
 
     public CQueue() {
-        stack1 = new LinkedList<Integer>();
-        stack2 = new LinkedList<Integer>();
+        stack1 = new LinkedList<>();
+        stack2 = new LinkedList<>();
     }
 
     public void appendTail(int value) {
@@ -31,4 +31,32 @@ public class CQueue {
 
 
 
+}
+ **********************************/
+class CQueue {
+
+    LinkedList<Integer> stack1 = new LinkedList<>();
+    LinkedList<Integer> stack2 = new LinkedList<>();
+
+    public CQueue() {
+
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        if (stack2.isEmpty()) {
+            return -1;
+        } else {
+            int deleteItem = stack2.pop();
+            return deleteItem;
+        }
+    }
 }
