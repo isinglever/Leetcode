e2ced452bb94ac1030ef49b606bfd0356cac67bc
Orikuo <isinglever@gmail.com>
2021-10-09 21:45:02 +0800

    add new questions

---
+++ String/FindSpace.java
@@ -0,0 +1,7 @@
+public class FindSpace {
+    public String replaceSpace(String s) {
+        s.replaceAll(" ", "%20");
+        return s;
+    }
+}
+//https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
