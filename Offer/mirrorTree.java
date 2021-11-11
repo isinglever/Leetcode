29288d7af7e32d7aa1535119e31bf63313754328
Orikuo <isinglever@gmail.com>
2021-11-11 14:43:52 +0800

    add mirrorTree.java

---
+++ Offer/mirrorTree.java
@@ -0,0 +1,16 @@
+public class mirrorTree {
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = mirrorTree(root.right);
+        root.right = mirrorTree(temp);
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+  }
+}
