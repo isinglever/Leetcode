3d93aa2eb5d22f06fdab83fc3d2cf9c755a7a257
Ori <isinglever@gmail.com>
2021-11-12 20:46:30 +0800

    just for test

---
+++ Offer/Exchange21.java
@@ -0,0 +1,13 @@
+public class Exchange21 {
+    public int[] exchange(int[] nums) {
+        int[] temp = new int[nums.length];
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]%2 !=0 ) j++;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]%2 !=0 )
+        }
+        return nums;
+    }
+}
