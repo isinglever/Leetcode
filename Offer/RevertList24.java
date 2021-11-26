12d52b84cc2e0509ac1db52895e1ec7e28cf92e6
Ori <isinglever@gmail.com>
2021-11-26 21:18:47 +0800

    add reverse linked list problem

--- Offer/RevertList24.java
+++ Offer/RevertList24.java
@@ -1,13 +1,24 @@
 public class RevertList24 {
+//    Using Iteration
+//    public ListNode reverseList(ListNode head) {
+//        ListNode cur = head, tmp, pre = null;
+//        while (cur != null) {
+//            tmp = cur.next;
+//            cur.next = pre;
+//            pre = cur;
+//            cur = tmp;
+//        }
+//        return pre;
+//    }
+
     public ListNode reverseList(ListNode head) {
-        ListNode cur = head, tmp, pre = null;
-        while (cur != null) {
-            tmp = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = tmp;
-        }
-        return pre;
+        return recur(head, null);
+    }
+    private ListNode recur(ListNode cur, ListNode pre) {
+        if (cur ==null) return pre;
+        ListNode res = recur(cur.next, cur);
+        cur.next = pre;
+        return res;
     }
     public class ListNode {
         int val;
