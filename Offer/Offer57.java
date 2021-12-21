895ffcf5712cd0acb4c37497ac5dacc3f686ff13
Ori <isinglever@gmail.com>
2021-12-21 19:26:10 +0800

    add a new problem solution

---
+++ Offer/Offer57.java
@@ -0,0 +1,30 @@
+public class Offer57 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] < target - nums[j]) i++;
+            if (nums[i] > target - nums[j]) j--;
+            if (nums[i] == target - nums[j]) {
+                ans[0] = nums[i];
+                ans[1] = nums[j];
+                return ans;
+            }
+        }
+        return null;
+    }
+}
+/*
+        if (nums.length < 2) return null;
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            ans[0] = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (ans[0] + nums[j] == target) {
+                    ans[1] = nums[j];
+                    return ans;
+                }
+            }
+        }
+        return ans;
+*/
