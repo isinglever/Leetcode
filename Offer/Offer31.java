d5ac99e7d6f0b99e99c0c64870244918438bf14f
Ori <isinglever@gmail.com>
2022-02-13 13:15:05 +0800

    add new solution

--- Offer/Offer31.java
+++ Offer/Offer31.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Offer31 {
+import java.util.Stack;
+
+public class Offer31 {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stc = new Stack<>();
+        int i = 0;
+        for (int num : pushed) {
+            stc.push(num);
+            while (!stc.isEmpty() && stc.peek() == popped[i]) {
+                stc.pop();
+                i++;
+            }
+        }
+        return stc.isEmpty();
+    }
 }
